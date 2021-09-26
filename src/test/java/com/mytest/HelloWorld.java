029166233042a4def677ec42e36f21b59b2c8c10
Pradyumnna Satapathy <dj.pradyumnna@gmail.com>
2021-09-26 18:04:25 +0530

    First TestApplication commit

---
+++ src/test/java/com/mytest/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.mytest;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+		@Test
+		public void testHelloWorld() {
+			System.out.println("Hello World");
+		}
+		
+	
+}
